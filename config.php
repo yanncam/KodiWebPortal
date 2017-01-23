505cce43b80ea58d8350111e3dcb62714f1c7af7
Yann CAM <yanncam@users.noreply.github.com>
2017-01-23 20:00:35 +0100

    German langage added. V1.0.5.
     Thank you for your contribution :) !

--- config.php
+++ config.php
@@ -79,7 +79,7 @@ define("NAX_TVSHOW_VIEW","tvshow_view");
 define("NAX_TVSHOWSEASON_VIEW","season_view");
 define("NAX_TVSHOWEPISODE_VIEW","episode_view");
 define("ENABLE_AUTHENTICATION", (ENABLE_INTERNAL_AUTHENTICATION || ENABLE_LDAP_AUTHENTICATION));
-define("KODI_WEB_PORTAL_VERSION", "1.0.4");
+define("KODI_WEB_PORTAL_VERSION", "1.0.5");
 define("IS_INCLUDED", true);
 
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
